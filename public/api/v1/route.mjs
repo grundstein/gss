a1a08455132e9df33cb0d9efa01b0a784fe16591
jascha <jascha@jaeh.at>
2020-09-27 23:24:42 +0200

    update public dir

--- public/api/v1/route.mjs
+++
@@ -1,9 +0,0 @@
-export default (req, res) => {
-  const random = Math.floor(Math.random() * 1000)
-  const body = `hello, world. here is some pseudorandom: ${random}`
-
-  return {
-    code: 200,
-    body,
-  }
-}
