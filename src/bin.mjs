120e48c4e024021029ff8874d75ac19f3c60a090
jascha <jascha@jaeh.at>
2020-10-20 00:16:45 +0200

    bin: change help output

--- src/bin.mjs
+++ src/bin.mjs
@@ -22,14 +22,14 @@ const opts = {
   },
   single: ['--dir', '--host', '--port', '--cors-origin', '--cors-headers'],
   help: {
-    name: 'gms: grundstein magic server',
-    header: 'serves static pages from memory.',
+    name: 'gss: grundstein static server',
+    header: 'serves static files from disk via streams.',
     options: {
-      '--dir': 'root for both api and static directories',
-      '--host': 'hostname to listen to, default 127.0.0.1',
-      '--port': 'port, default 2350',
-      '--cors-origin': 'value of the Access-Control-Allow-Origin http head',
-      '--cors-headers': 'value of the Access-Controll-Allow-Headers http head',
+      '--dir': 'root for static file directory',
+      '--host': 'hostname to listen to',
+      '--port': 'port to listen to',
+      '--cors-origin': 'value of the Access-Control-Allow-Origin http header',
+      '--cors-headers': 'value of the Access-Controll-Allow-Headers http header',
     },
     example: `
 # serve files in ./api:
