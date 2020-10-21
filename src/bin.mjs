9fbf1ccaaf646e79fb01983cbba01b068b93e346
jascha <jascha@jaeh.at>
2020-10-21 20:49:14 +0200

    src/bin: add --proxies arg

--- src/bin.mjs
+++ src/bin.mjs
@@ -8,8 +8,9 @@ const opts = {
   options: [
     ['--help', '-help', 'help', '--h', '-h'],
     ['--dir', '-d'],
-    ['--host', '-n'],
+    ['--host', '--hostname', '-n'],
     ['--port', '-p'],
+    ['--proxies', '--proxy'],
     ['--cors-origin', '--cors', '-c'],
     ['--cors-headers'],
   ],
@@ -17,6 +18,7 @@ const opts = {
     '--dir': 'public',
     '--host': '127.0.0.1',
     '--port': 2350,
+    '--proxies': 'localhost',
     '--cors-origin': false,
     '--cors-headers': 'Origin, X-Requested-With, Content-Type, Accept',
   },
@@ -28,15 +30,16 @@ const opts = {
       '--dir': 'root for static file directory',
       '--host': 'hostname to listen to',
       '--port': 'port to listen to',
+      '--proxies': 'hostnames to proxy to directory',
       '--cors-origin': 'value of the Access-Control-Allow-Origin http header',
       '--cors-headers': 'value of the Access-Controll-Allow-Headers http header',
     },
     example: `
-# serve files in ./api:
+# serve host separated files in ./public/ for host "localhost":
 gms
 
-# serve files using an absolute path, custom host and port.
-gms --dir /api --host grundstein.it --port 2323
+# serve files using an absolute path, custom host, port, and proxies.
+gms --dir /var/www/html --host grundstein.it --port 2323 --proxies example.org example.com
 `,
   },
 }
