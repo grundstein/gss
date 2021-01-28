5eb06c539e0e2b53530b66c071fd3431cb13d1e8
jaeh <jascha@jaeh.at>
2021-01-28 05:58:04 +0100

    bin: use ENV variables as input too

--- src/bin.mjs
+++ src/bin.mjs
@@ -4,6 +4,16 @@ import cli from '@magic/cli'
 
 import run from './index.mjs'
 
+const {
+  GSS_DIR = '/var/www/html',
+  GSS_HOST = '0.0.0.0',
+  GSS_PORT = 2350,
+  GSS_CERT_DIR = '/home/grundstein/ca',
+  GSS_PROXY_FILE = '/home/grundstein/proxies',
+  GSS_CORS_ORIGIN = false,
+  GSS_CORS_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept',
+} = process.env
+
 const opts = {
   options: [
     ['--help', '-help', 'help', '--h', '-h'],
@@ -15,13 +25,13 @@ const opts = {
     ['--cors-headers'],
   ],
   default: {
-    '--dir': '/var/www/html',
-    '--host': '0.0.0.0',
-    '--port': 2350,
-    '--cert-dir': '/home/grundstein/ca',
-    '--proxy-file': '/home/grundstein/proxies',
-    '--cors-origin': false,
-    '--cors-headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    '--dir': GSS_DIR,
+    '--host': GSS_HOST,
+    '--port': GSS_PORT,
+    '--cert-dir': GSS_CERT_DIR,
+    '--proxy-file': GSS_PROXY_FILE,
+    '--cors-origin': GSS_CORS_ORIGIN,
+    '--cors-headers': GSS_CORS_HEADERS,
   },
   single: ['--dir', '--host', '--port', '--cors-origin', '--cors-headers', '--proxy-file'],
   help: {
