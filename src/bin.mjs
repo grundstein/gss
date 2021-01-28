bd4877e5d10e87095802244cff3e0eeecbed159a
jaeh <jascha@jaeh.at>
2021-01-28 06:52:05 +0100

    bin: read .env file before starting to get env vars

--- src/bin.mjs
+++ src/bin.mjs
@@ -3,58 +3,76 @@
 import cli from '@magic/cli'
 
 import run from './index.mjs'
+import fs from '@magic/fs'
 
-const {
-  GSS_DIR = '/var/www/html',
-  GSS_HOST = '0.0.0.0',
-  GSS_PORT = 2350,
-  GSS_CERT_DIR = '/home/grundstein/ca',
-  GSS_PROXY_FILE = '/home/grundstein/proxies',
-  GSS_CORS_ORIGIN = '*',
-  GSS_CORS_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept',
-} = process.env
+const prepare = async () => {
+  const GSS_ENV_FILE = '/home/grundstein/.env'
 
-const opts = {
-  options: [
-    ['--help', '-help', 'help', '--h', '-h'],
-    ['--dir', '-d'],
-    ['--host', '-n'],
-    ['--port', '-p'],
-    ['--proxy-file'],
-    ['--cors-origin', '--cors', '-c'],
-    ['--cors-headers'],
-  ],
-  default: {
-    '--dir': GSS_DIR,
-    '--host': GSS_HOST,
-    '--port': GSS_PORT,
-    '--cert-dir': GSS_CERT_DIR,
-    '--proxy-file': GSS_PROXY_FILE,
-    '--cors-origin': GSS_CORS_ORIGIN,
-    '--cors-headers': GSS_CORS_HEADERS,
-  },
-  single: ['--dir', '--host', '--port', '--cors-origin', '--cors-headers', '--proxy-file'],
-  help: {
-    name: 'gss: grundstein static server',
-    header: 'serves static files from disk via streams.',
-    options: {
-      '--dir': 'root for static file directory',
-      '--host': 'hostname to listen to',
-      '--port': 'port to listen to',
-      '--proxy-file': 'file with a list of proxies, split on newlines',
-      '--cors-origin': 'value of the Access-Control-Allow-Origin http header',
-      '--cors-headers': 'value of the Access-Controll-Allow-Headers http header',
+  let env = { ...process.env }
+
+  const envExists = await fs.exists(GSS_ENV_FILE)
+  if (envExists) {
+    const ENV = await fs.readFile(GSS_ENV_FILE)
+    env = {
+      ...env,
+      ...JSON.parse(ENV),
+    }
+  }
+
+  const {
+    GSS_DIR = '/var/www/html',
+    GSS_HOST = '0.0.0.0',
+    GSS_PORT = 2350,
+    GSS_CERT_DIR = '/home/grundstein/ca',
+    GSS_PROXY_FILE = '/home/grundstein/proxies',
+    GSS_CORS_ORIGIN = '*',
+    GSS_CORS_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept',
+  } = env
+
+  const opts = {
+    options: [
+      ['--help', '-help', 'help', '--h', '-h'],
+      ['--dir', '-d'],
+      ['--host', '-n'],
+      ['--port', '-p'],
+      ['--proxy-file'],
+      ['--cors-origin', '--cors', '-c'],
+      ['--cors-headers'],
+    ],
+    default: {
+      '--dir': GSS_DIR,
+      '--host': GSS_HOST,
+      '--port': GSS_PORT,
+      '--cert-dir': GSS_CERT_DIR,
+      '--proxy-file': GSS_PROXY_FILE,
+      '--cors-origin': GSS_CORS_ORIGIN,
+      '--cors-headers': GSS_CORS_HEADERS,
     },
-    example: `
+    single: ['--dir', '--host', '--port', '--cors-origin', '--cors-headers', '--proxy-file'],
+    help: {
+      name: 'gss: grundstein static server',
+      header: 'serves static files from disk via streams.',
+      options: {
+        '--dir': 'root for static file directory',
+        '--host': 'hostname to listen to',
+        '--port': 'port to listen to',
+        '--proxy-file': 'file with a list of proxies, split on newlines',
+        '--cors-origin': 'value of the Access-Control-Allow-Origin http header',
+        '--cors-headers': 'value of the Access-Controll-Allow-Headers http header',
+      },
+      example: `
 # serve host separated files in ./public/ for host "localhost":
-gms
+gss
 
 # serve files using an absolute path, custom host, port, and proxies.
-gms --dir /var/www/html --host grundstein.it --port 2323 --proxies example.org example.com
+gss --dir /var/www/html --host grundstein.it --port 2323 --proxies example.org example.com
 `,
-  },
+    },
+  }
+
+  const { args } = cli(opts)
+
+  run(args)
 }
 
-const { args } = cli(opts)
-
-run(args)
+prepare()
