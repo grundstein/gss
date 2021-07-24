383f4bd37028f8320645bc35c6200bbccdf6585f
jaeh <jascha@jaeh.at>
2021-07-24 21:14:33 +0200

    format

--- src/handler.mjs
+++ src/handler.mjs
@@ -6,111 +6,106 @@ import mimeTypes from '@magic/mime-types'
 
 const { formatLog, getHostname, respond, sendStream } = lib
 
-export const handler = ({
-  dir,
-  corsOrigin,
-  corsHeaders,
-  proxies,
-  immutableFiletypes = [],
-}) => async (req, res) => {
-  const time = log.hrtime()
+export const handler =
+  ({ dir, corsOrigin, corsHeaders, proxies, immutableFiletypes = [] }) =>
+  async (req, res) => {
+    const time = log.hrtime()
 
-  let hostname = ''
-  if (proxies.length) {
-    hostname = getHostname(req)
+    let hostname = ''
+    if (proxies.length) {
+      hostname = getHostname(req)
 
-    if (!proxies.includes(hostname)) {
-      respond(req, res, { body: '403 - Invalid Hostname.', code: 403 })
-      return
-    }
-  }
-
-  let { url } = req
-  if (url.includes('?')) {
-    url = url.split('?')[0]
-  }
-  if (url.endsWith('/')) {
-    url = path.join(url, 'index.html')
-  } else {
-    const extname = path.extname(url)
-    if (!extname) {
-      url += '/'
-      respond(req, res, { code: 302, headers: { Location: url } })
-      return
-    }
-  }
-
-
-  let fullFilePath = path.join(dir, hostname, url)
-
-  const zipFilePath = `${fullFilePath}.gz`
-
-  let stat
-
-  try {
-    const clientAcceptedEncodings = req.headers['accept-encoding']
-    if (!clientAcceptedEncodings?.includes('gzip')) {
-      // this error gets swallowed
-      throw new Error('no encryption accepted.')
-    }
-
-    stat = await fs.stat(zipFilePath)
-    fullFilePath += '.gz'
-  } catch (_) {
-    try {
-      stat = await fs.stat(fullFilePath)
-    } catch (e) {
-      if (e.code !== 'ENOENT') {
-        log.error(e)
-        respond(req, res, { body: '500 - Unknown error.', code: 500 })
+      if (!proxies.includes(hostname)) {
+        respond(req, res, { body: '403 - Invalid Hostname.', code: 403 })
         return
       }
     }
-  }
 
-  if (stat) {
-    const mimeExtension = path.extname(url).substr(1)
-    const headers = {}
-
-    if (fullFilePath.endsWith('.gz')) {
-      headers['Content-Encoding'] = 'gzip'
+    let { url } = req
+    if (url.includes('?')) {
+      url = url.split('?')[0]
+    }
+    if (url.endsWith('/')) {
+      url = path.join(url, 'index.html')
+    } else {
+      const extname = path.extname(url)
+      if (!extname) {
+        url += '/'
+        respond(req, res, { code: 302, headers: { Location: url } })
+        return
+      }
     }
 
-    const file = {
-      size: stat.size,
-      mime: mimeTypes[mimeExtension],
-      path: fullFilePath,
-    }
+    let fullFilePath = path.join(dir, hostname, url)
 
-    if (corsOrigin) {
-      let val = '*'
-      if (corsOrigin !== '*') {
-        const forwardedFor = req.headers['x-forwarded-for']
-        if (forwardedFor && corsOrigin.includes(forwardedFor)) {
-          val = forwardedFor
-        }
+    const zipFilePath = `${fullFilePath}.gz`
+
+    let stat
+
+    try {
+      const clientAcceptedEncodings = req.headers['accept-encoding']
+      if (!clientAcceptedEncodings?.includes('gzip')) {
+        // this error gets swallowed
+        throw new Error('no encryption accepted.')
       }
 
-      headers['Access-Control-Allow-Origin'] = val
-      headers['Access-Control-Allow-Headers'] = corsHeaders
+      stat = await fs.stat(zipFilePath)
+      fullFilePath += '.gz'
+    } catch (_) {
+      try {
+        stat = await fs.stat(fullFilePath)
+      } catch (e) {
+        if (e.code !== 'ENOENT') {
+          log.error(e)
+          respond(req, res, { body: '500 - Unknown error.', code: 500 })
+          return
+        }
+      }
     }
 
-    let maxAge = '600'
+    if (stat) {
+      const mimeExtension = path.extname(url).substr(1)
+      const headers = {}
 
-    const isImmutable = immutableFiletypes.some(f => file.path.includes(`.${f}`))
+      if (fullFilePath.endsWith('.gz')) {
+        headers['Content-Encoding'] = 'gzip'
+      }
 
-    if (isImmutable) {
-      const secondsToCache = 60 * 60 * 24 * 365 // one year
-      maxAge = `max-age=${secondsToCache}, immutable`
+      const file = {
+        size: stat.size,
+        mime: mimeTypes[mimeExtension],
+        path: fullFilePath,
+      }
+
+      if (corsOrigin) {
+        let val = '*'
+        if (corsOrigin !== '*') {
+          const forwardedFor = req.headers['x-forwarded-for']
+          if (forwardedFor && corsOrigin.includes(forwardedFor)) {
+            val = forwardedFor
+          }
+        }
+
+        headers['Access-Control-Allow-Origin'] = val
+        headers['Access-Control-Allow-Headers'] = corsHeaders
+      }
+
+      let maxAge = '600'
+
+      const isImmutable = immutableFiletypes.some(f => file.path.includes(`.${f}`))
+
+      if (isImmutable) {
+        const secondsToCache = 60 * 60 * 24 * 365 // one year
+        maxAge = `max-age=${secondsToCache}, immutable`
+      }
+
+      headers['Cache-Control'] = `public, ${maxAge}`
+
+      sendStream(req, res, { file, headers })
+      formatLog(req, res, { time, type: 'static' })
+      return
     }
 
-    headers['Cache-Control'] = `public, ${maxAge}`
-
-    sendStream(req, res, { file, headers })
-    formatLog(req, res, { time, type: 'static' })
-    return
+    respond(req, res, { body: '404 - not found.', code: 404 })
+    formatLog(req, res, { time, type: '404' })
   }
-
-  respond(req, res, { body: '404 - not found.', code: 404 })
-  formatLog(req, res, { time, type: '404' })
-}
