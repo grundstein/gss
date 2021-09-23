e47114385cfe901e2f0aece9eee31ccedb846f2e
jaeh <jascha@jaeh.at>
2021-09-23 22:22:20 +0200

    update test/etags to use lib from @grundstein/commons

--- test/etags.mjs
+++ test/etags.mjs
@@ -2,17 +2,16 @@ import path from 'path'
 
 import { fs, is } from '@magic/test'
 
-import { etags } from '../src/etags.mjs'
+import { lib } from '@grundstein/commons'
 
 const indexHtmlPath = path.join(process.cwd(), 'public', 'index.html')
 const stat = await fs.statSync(indexHtmlPath)
 const indexHtmlExpect = (stat.size + stat.mtimeMs).toString(36)
 
 export default [
-  { fn: etags(), expect: is.fn, info: 'returns a function when called with empty argument' },
   {
     fn: async () => {
-      const e = await etags('public')
+      const e = await lib.etags('public')
       return e({ file: 'public/index.html', stat })
     },
     expect: indexHtmlExpect,
