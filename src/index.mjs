c94c94a74a488046dc662521fa9bb8f8433f0bc4
jaeh <jascha@jaeh.at>
2021-09-23 22:18:15 +0200

    use @grundstein/commons/lib.etags instead of local implementation

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,13 @@
 import { lib, log } from '@grundstein/commons'
 
 import { handler } from './handler.mjs'
-import { etags } from './etags.mjs'
 
 export const run = async (config = {}) => {
   try {
     const args = {
       startTime: log.hrtime(),
       proxies: await lib.getProxies(config),
-      etag: await etags(config.dir),
+      etag: await lib.etags(config.dir),
       ...config,
     }
 
