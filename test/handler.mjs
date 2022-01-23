ea69a7881a536727a1f0db5cff80e76395d5fcef
jascha <j@jaeh.at>
2022-01-23 15:19:02 +0100

    test/handler: handler does not error without arguments

--- test/handler.mjs
+++ test/handler.mjs
@@ -3,6 +3,10 @@ import { is, tryCatch } from '@magic/test'
 import { handler } from '../src/handler.mjs'
 
 export default [
-  { fn: tryCatch(handler), expect: is.error, info: 'calling the handler without arguments errors' },
+  {
+    fn: tryCatch(handler),
+    expect: t => !is.error(t),
+    info: 'calling the handler without arguments works fine',
+  },
   { fn: () => handler({}), expect: is.fn, info: 'calling the handler returns a function' },
 ]
