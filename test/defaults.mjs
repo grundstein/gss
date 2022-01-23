50eed6f70a54aa799c1129b3cbd0dd13fa96b596
jascha <j@jaeh.at>
2022-01-23 16:22:55 +0100

    update test/defaults

--- test/defaults.mjs
+++ test/defaults.mjs
@@ -1,4 +1,4 @@
-import { is } from '@magic/test'
+import { is, version } from '@magic/test'
 
 import { defaults } from '../src/defaults.mjs'
 
@@ -13,8 +13,24 @@ const expected = {
   immutableFileTypes: ['glb', 'mp4', 'webm', 'mp3'],
   cache: false,
   etag: false,
+  proxies: [],
+}
+
+const spec = {
+  dir: is.str,
+  host: is.str,
+  port: is.num,
+  certDir: is.str,
+  proxyFile: is.str,
+  corsOrigin: is.str,
+  corsHeaders: is.str,
+  immutableFileTypes: is.arr,
+  cache: is.bool,
+  etag: is.bool,
+  proxies: is.arr,
 }
 
 export default [
   { fn: is.deep.equal(expected, defaults), info: 'defaults.mjs exports have changed.' },
+  ...version(defaults, spec),
 ]
