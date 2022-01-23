a5e5e200d5b855b3508adc4325d5af0304f3ab1a
jascha <j@jaeh.at>
2022-01-23 15:41:05 +0100

    add src/defaults

---
+++ src/defaults.mjs
@@ -0,0 +1,12 @@
+export const defaults = {
+  dir: '/var/www/html',
+  host: '0.0.0.0',
+  port: 2350,
+  certDir: '/home/grundstein/ca',
+  proxyFile: '/home/grundstein/proxies',
+  corsOrigin: '*',
+  corsHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  immutableFileTypes: ['glb', 'mp4', 'webm', 'mp3'],
+  cache: false,
+  etag: false,
+}
